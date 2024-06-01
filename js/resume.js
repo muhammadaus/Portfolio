1401fdfda88e3e759f74c06656b31e498300d847
MUHAMMAD AUS HIJRI <aushijree@gmail.com>
2024-06-01 18:05:39 +0900

    Merged master

--- js/resume.js
+++ js/resume.js
@@ -1,1 +1,29 @@
-// Include JavaScript Functionality Here...
+// document.getElementById('close-intro').addEventListener('click', function() {
+//     document.querySelector('.blur-background').style.display = 'none';
+//     document.querySelector('.intro-screen').style.display = 'none';
+// });
+
+document.body.classList.add('lock-scroll');
+
+document.getElementById('close-intro').addEventListener('click', function() {
+    document.body.classList.remove('lock-scroll');
+    document.querySelector('.intro-screen').style.display = 'none';
+    var elements = document.querySelectorAll('body > *:not(.intro-screen)');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = 'block';
+    }
+});
+
+$(document).ready(function(){
+    $('a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        }
+    });
+});
